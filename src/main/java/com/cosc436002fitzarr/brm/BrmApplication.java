45502e38c6d44e796974308a8eb874c91d55751b
GeoffA12 <Geoffarroyo@gmail.com>
2020-10-01 03:47:26 -0500

    Initial Commit With Spring Boot Project Dependencies.

---
+++ src/main/java/com/cosc436002fitzarr/brm/BrmApplication.java
@@ -0,0 +1,13 @@
+package com.cosc436002fitzarr.brm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosc436002fitzarr/brm/BrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosc436002fitzarr.brm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

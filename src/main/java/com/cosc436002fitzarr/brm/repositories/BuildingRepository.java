33451c677b83a561fb9e70f9ab0ea4c1ecbf4b56
GeoffA12 <Geoffarroyo@gmail.com>
2021-01-29 04:28:51 -0600

    Refactoring find buildings by site method in the building service so that it uses one bluk query.

--- src/main/java/com/cosc436002fitzarr/brm/repositories/BuildingRepository.java
+++ src/main/java/com/cosc436002fitzarr/brm/repositories/BuildingRepository.java
@@ -2,11 +2,13 @@ package com.cosc436002fitzarr.brm.repositories;
 
 import com.cosc436002fitzarr.brm.models.building.Building;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BuildingRepository extends MongoRepository<Building, String> {
-    public List<Building> findBuildingsBySiteId(String siteId);
+    @Query(value = "{ 'siteId': { $in: ?0 }}")
+    public List<Building> findBuildingsByAssociatedSiteIds(List<String> associatedSiteIds);
 }
